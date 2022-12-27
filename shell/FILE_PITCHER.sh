eac57a90909b455bc030e34e9a2c81181610c530
madeinkgb@gmail.com <madeinkgb@gmail.com>
2022-12-28 08:49:21 +0900

    update

--- shell/FILE_PITCHER.sh
+++ shell/FILE_PITCHER.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 APP_PNM=UMS_FILE_PITCHER
-APP_PID=300_$APP_PNM
+APP_PID=301_$APP_PNM
 PIDS=""
 export APP_PNM=$APP_PNM
 . ./env.sh
-APP_LOGF=$APP_LOG/$APP_PNM.log
+APP_LOGF=$APP_LOG/$APP_PNM
 runutil $APP_PID $1 "$2"
 
 nohup $APP_JAVA $UMS_JVMOPT -Dpnm=$APP_PID $LOGBACK -server -jar $APP_JAR $APP_CONF_FILE > $APP_LOGF.out 2>&1 &
